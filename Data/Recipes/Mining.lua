8702d74fbeb7bdc2830c465950030fe3ce95f3f5
squall#69 <squuall123@gmail.com>
2025-08-27 21:24:50 +0200

    fix(professions) added mining data table

---
+++ Data/Recipes/Mining.lua
@@ -0,0 +1,83 @@
+-- Mining.lua
+-- Complete Mining recipe database
+
+local _, addon = ...
+
+if not addon.VanillaAccurateData then
+    addon.VanillaAccurateData = {}
+end
+
+addon.VanillaAccurateData[186] = {
+    [2840] = {
+        difficulty = {},
+        itemId = 2840,
+        skill = 25,
+        spellId = 2657,
+    },
+    [2842] = {
+        difficulty = {},
+        itemId = 2842,
+        skill = 25,
+        spellId = 2658,
+    },
+    [2841] = {
+        difficulty = {},
+        itemId = 2841,
+        skill = 25,
+        spellId = 2659,
+    },
+    [3576] = {
+        difficulty = {},
+        itemId = 3576,
+        skill = 25,
+        spellId = 3304,
+    },
+    [3575] = {
+        difficulty = {},
+        itemId = 3575,
+        skill = 25,
+        spellId = 3307,
+    },
+    [3577] = {
+        difficulty = {},
+        itemId = 3577,
+        skill = 25,
+        spellId = 3308,
+    },
+    [3859] = {
+        difficulty = {},
+        itemId = 3859,
+        skill = 25,
+        spellId = 3569,
+    },
+    [3860] = {
+        difficulty = {},
+        itemId = 3860,
+        skill = 25,
+        spellId = 10097,
+    },
+    [6037] = {
+        difficulty = {},
+        itemId = 6037,
+        skill = 25,
+        spellId = 10098,
+    },
+    [11371] = {
+        difficulty = {},
+        itemId = 11371,
+        skill = 25,
+        spellId = 14891,
+    },
+    [12359] = {
+        difficulty = {},
+        itemId = 12359,
+        skill = 25,
+        spellId = 16153,
+    },
+    [17771] = {
+        difficulty = {},
+        itemId = 17771,
+        skill = 25,
+        spellId = 22967,
+    },
+}
